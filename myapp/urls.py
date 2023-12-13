c30d572272db2563c4e81e04b5ca49e949882d25
amishasharma0912@gmail.com <amishasharma0912@gmail.com>
2023-12-13 16:40:50 +0530

    merge all remote updates

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,15 @@
 # myapp/urls.py
 from django.urls import path
 from .views import UserRegistrationView, UserLoginView
+from .views import UserRegistrationView, UserLoginView, ContentGenerationView
+from .views import UserRegistrationView, UserLoginView, ContentGenerationView, ImageRecognitionView
+
 
 urlpatterns = [
     path('api/register/', UserRegistrationView.as_view(), name='register'),
     path('api/login/', UserLoginView.as_view(), name='login'),
+    path('api/content-generation/', ContentGenerationView.as_view(), name='content_generation'),
+    path('api/image-recognition/', ImageRecognitionView.as_view(), name='image_recognition'),
+
+
 ]
